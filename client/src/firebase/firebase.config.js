528243c600ff47ccff2d07577d2a638c7c727d4e
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-04-25 23:34:05 +0600

    card components added

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyAcHK-S5kib6nxCimmGuQTgTUPMGcZM5ag",
+  authDomain: "stay-hotel-a3a0a.firebaseapp.com",
+  projectId: "stay-hotel-a3a0a",
+  storageBucket: "stay-hotel-a3a0a.appspot.com",
+  messagingSenderId: "83200762756",
+  appId: "1:83200762756:web:302bfccfaee83a949873d1"
 }
 
 export const app = initializeApp(firebaseConfig)
