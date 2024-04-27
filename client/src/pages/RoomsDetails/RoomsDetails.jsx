9ad7c434b7c2c702ff531c93302a42a32b4414c0
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-04-28 00:33:10 +0600

    react calender setup

--- client/src/pages/RoomsDetails/RoomsDetails.jsx
+++ client/src/pages/RoomsDetails/RoomsDetails.jsx
@@ -3,6 +3,9 @@ import Container from "../../components/Shared/Container";
 import { useEffect, useState } from "react";
 import Loader from "../../components/Shared/Loader";
 import { Helmet } from "react-helmet-async";
+import Header from "../../components/RoomDetails/Header";
+import RoomInfo from "../../components/RoomDetails/RoomInfo/RoomInfo";
+import Calender from "../../components/RoomDetails/Calender";
 
 const RoomsDetails = () => {
   const { id } = useParams();
@@ -19,13 +22,27 @@ const RoomsDetails = () => {
         setLoading(false);
       });
   }, [id]);
-  if (loading) return <Loader />;
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <>
-        <Helmet>
-            <title>{room.title}</title>
-        </Helmet>
-      <Container>{room.title}</Container>
+      <Helmet>
+        <title>{room.title}</title>
+      </Helmet>
+      <Container>
+        <div className="max-w-screen-lg mx-auto">
+          <div className="flex flex-col gap-6">
+            <Header room={room} />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-8">
+            <RoomInfo room={room} />
+            <div className="md:col-span-3 order-first md:order-last mb-10">
+              <Calender />
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
