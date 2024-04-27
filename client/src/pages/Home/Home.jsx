88ed57bf3a063d84e1cf529946eacb916f94f537
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-04-27 23:04:32 +0600

    react helmet setup

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
-import Categories from "../../components/Rooms/Categories/Categories"
-import Rooms from "../../components/Rooms/Rooms"
+import { Helmet } from "react-helmet-async";
+import Categories from "../../components/Rooms/Categories/Categories";
+import Rooms from "../../components/Rooms/Rooms";
 
 const Home = () => {
   return (
     <div>
-      <Categories/>
-      <Rooms/>
+      <Helmet>
+        <title>StayVista | Vacation Homes & Condo Rentals</title>
+      </Helmet>
+      <Categories />
+      <Rooms />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- client/src/pages/RoomsDetails/RoomsDetails.jsx
+++ client/src/pages/RoomsDetails/RoomsDetails.jsx
@@ -1,11 +1,33 @@
+import { useParams } from "react-router-dom";
 import Container from "../../components/Shared/Container";
+import { useEffect, useState } from "react";
+import Loader from "../../components/Shared/Loader";
+import { Helmet } from "react-helmet-async";
 
 const RoomsDetails = () => {
-    return (
-        <Container>
-            Rooms Details
-        </Container>
-    );
+  const { id } = useParams();
+  const [room, setRoom] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/rooms.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const signleRoom = data.find((room) => room._id === id);
+        setRoom(signleRoom);
+        setLoading(false);
+      });
+  }, [id]);
+  if (loading) return <Loader />;
+  return (
+    <>
+        <Helmet>
+            <title>{room.title}</title>
+        </Helmet>
+      <Container>{room.title}</Container>
+    </>
+  );
 };
 
 export default RoomsDetails;
